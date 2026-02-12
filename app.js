b7229f74c5223569b3dfab4dd9587f950f4b695e
Sahil Nerkar <sahilnerkar0@gmail.com>
2026-02-13 01:30:12 +0530

    validation changes done

--- app.js
+++ app.js
@@ -1,85 +1,92 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
+// const Listing = require("./models/listing.js"); // Comment out to prevent mongoose connection issues
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema , reviewSchema} = require("./schema.js");
+// const Review = require("./models/review.js"); // Comment out to prevent mongoose connection issues
+const { required } = require("joi");
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
+const mockData = require("./utils/mockData.js");
 
+// Replace MongoDB connection with mock data
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// For now, since MongoDB is not installed, we'll use mock data
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-async function main() {
-  await mongoose.connect(MONGO_URL);
-}
+// Connect to MongoDB
+// main()
+//   .then(() => {
+//     console.log("connected to DB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async function main() {
+//   await mongoose.connect(MONGO_URL);
+// }
+
+// For now, just log that we're ready
+console.log("App initialized with mock data");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname,"/public")));
+
+const sessionOptions = {
+  secret: "Mysupersecretcode",
+  resave : false,
+  saveUninitialized : true,
+  cookie: {
+    expires :  Date.now() +  7 * 24 * 60 *60 * 1000,
+    maxAge :  7 * 24 * 60 *60 * 1000,
+    httpOnly : true,
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
-//Index Route
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res , next)=>{
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-//New Route
-app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
+app.use("/listings",listings);
+app.use("/listings/:id/reviews", reviews);
+
+
+app.all("*", (req, res , next )=> {
+  next(new ExpressError(404, "Page Not Found"));
 });
 
-//Show Route
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
-
-//Create Route
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
-
-//Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
-
-//Update Route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
-
-//Delete Route
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
-});
-
+app.use((err, req , res , next )=>{
+  let{statusCode=500 , message="Something went wrong" }= err;
+  res.status(statusCode).render("error.ejs",{message});
+   //res.status(statusCode).send(message);
+})
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
 });
+
+
+
