e1bda92b68eeeeb82431709fcfb164c750754573
Sahil Nerkar <sahilnerkar0@gmail.com>
2026-03-22 17:15:23 +0530

    done with routes

--- models/listing.js
+++ models/listing.js
@@ -12,26 +12,27 @@ const listingSchema = new Schema({
     filename: String,
     url: {
       type: String,
-      default:
-        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?auto=format&fit=crop&w=800&q=60",
       set: (v) =>
         v === ""
-          ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?auto=format&fit=crop&w=800&q=60"
           : v,
     },
   },
   price: Number,
   location: String,
   country: String,
-  reviews : [{
-            type: Schema.Types.ObjectId,
-            ref: "Review",
-  },],
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
-listingSchema.post("findOneAndDelete", async function (listing) {
-  if (listing) {
-    await Review.deleteMany({ _id: { $in: listing.reviews } });
+listingSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({ _id: { $in: doc.reviews } });
   }
 });
 
